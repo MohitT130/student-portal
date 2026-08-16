57802dd67f6535461381f6f5b3fdebc94b2a02a4
Mohit Tripathi <mohitt9794@gmail.com>
2026-08-16 19:15:32 +0530

    feat(profile): display full student profile details

--- src/profile.cpp
+++ src/profile.cpp
@@ -9,11 +9,15 @@ struct StudentProfile {
 
 void displayProfile(const StudentProfile& profile) {
     std::cout << "Name: " << profile.name << std::endl;
+    std::cout << "Roll Number: " << profile.rollNumber << std::endl;
+    std::cout << "Department: " << profile.department << std::endl;
 }
 
 int main() {
     StudentProfile profile;
     profile.name = "Mohit Tripathi";
+    profile.rollNumber = "2026204020";
+    profile.department = "Computer Science";
     displayProfile(profile);
     return 0;
 }
