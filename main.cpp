a8291fc6000a19703ac5085031afccb0a4dbc8ec
Mohit Tripathi <mohitt9794@gmail.com>
2026-08-16 17:20:50 +0530

    feat: add greetUser function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
 void printWelcomeMessage() {
-   std::cout <<"     Welcome to Git !     " << std::endl;
+   std::cout<< "    Welcome to Git!     " << std::endl;
 }
 
-int main(){
-    printWelcomeMessage();
-    return 0;
+void greetUser() [
+   std::cout<< "Hello, Developer! << std::endl;
+}
+
+int main() {
+   printWelcomeMessage();
+   greetUser();
+return 0;
 }
